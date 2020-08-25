1ce8dac29394ab92e6deb1df26a3aa77ed322de9
hd4niel <daniel.holmberg97@gmail.com>
2020-08-25 14:54:41 +0300

    Speed up theme changing

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -2,14 +2,14 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import * as React from 'react'
 
-import { withTheme, getNavigatorTheme } from '../hooks/useTheme'
+import {getNavigatorTheme } from '../hooks/useTheme'
 import { RootStackParamList } from '../types'
 import NotFoundScreen from '../screens/NotFoundScreen'
 import BottomTabNavigator from './BottomTabNavigator'
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
-function Navigation () {
+export default function Navigation () {
     return (
         <NavigationContainer
             theme={getNavigatorTheme()}>
@@ -17,8 +17,6 @@ function Navigation () {
         </NavigationContainer>
     )
 }
-
-export default withTheme(Navigation)
 
 // A root stack navigator is often used for displaying modals on top of all other content
 // Read more here: https://reactnavigation.org/docs/modal
