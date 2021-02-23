04c015c3351718f46238bbf90ec0161fc00370f9
deinal <daniel.holmberg97@gmail.com>
2021-02-23 13:10:57 +0200

    Expo upgrade

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { YellowBox } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { ThemeContextProvider } from './hooks/useTheme'
@@ -7,14 +6,6 @@ import useCachedResources from './hooks/useCachedResources'
 import Navigation from './navigation'
 
 export default function App () {
-    // Prevent timer, open issue (react-native/issues/12981)
-    YellowBox.ignoreWarnings(['Setting a timer'])
-    const _console = { ...console }
-    console.warn = message => {
-        if (message.indexOf('Setting a timer') <= -1) {
-            _console.warn(message)
-        }
-    }
 
     // SplashScreen while loading assets
     const isLoadingComplete = useCachedResources()
